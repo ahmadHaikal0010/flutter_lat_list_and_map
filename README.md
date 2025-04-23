c770dc72c3821df30f6d7a4d7d106ccd35a07dd5
Ahmad Haikal <haikala0010@gmail.com>
2025-04-23 15:12:59 +0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_lat_list_and_map_mi2a
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_lat_list_and_map_mi2a/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_lat_list_and_map_mi2a
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
